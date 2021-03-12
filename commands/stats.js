e6875559642daa9eed981e62e4b51ee3f10d5606
juiceboy <porterpmilton@gmail.com>
2021-03-11 23:30:14 -0600

    formatted by hen

--- commands/stats.js
+++ commands/stats.js
@@ -41,70 +41,66 @@ module.exports = {
 
 			// search for game type (if any, else, general stats)
 			if(args[1]) {
-			switch (args[1]) {
-				case 'skywars':
-				case 'sw':
-					embed.setAuthor('Skywars Stats');
-					embed.setDescription(nameFormatted);
-					embed.addFields(
-					{ name:'Level', value: game.skywars.levelFormatted, inline: true },
-					{ name:'Prestige', value: game.skywars.prestige, inline: true },
-					{ name:'Played Games', value: game.skywars.playedGames, inline: true },
-					{ name:'Experience', value: game.skywars.experience, inline: true },
-					{ name:'XP Needed', value: game.skywars.levelProgress.xpToNextLevel, inline: true },
-					{ name:'Percent', value: game.skywars.levelProgress.percent, inline: true },
-					{ name:'Wins', value: game.skywars.wins, inline: true },
-					{ name:'Losses', value: game.skywars.losses, inline: true },
-					{ name:'WLR', value: game.skywars.WLRatio, inline: true },
-					{ name:'Kills', value: game.skywars.kills, inline: true },
-					{ name:'Deaths', value: game.skywars.deaths, inline: true },
-					{ name:'KDR', value: game.skywars.KDRatio, inline: true },
-					{ name:'Coins', value: game.skywars.coins, inline: true },
-					{ name:'Heads', value: game.skywars.heads, inline: true },
-					{ name:'Souls', value: game.skywars.souls, inline: true },
-					{ name:'Level Progress', value: genBar(game.skywars.levelProgress.percent), inline: false });
-					break;
+                switch (args[1]) {
+                    case 'skywars':
+                    case 'sw':
+                        embed.setAuthor('Skywars Stats');
+                        embed.setDescription(nameFormatted);
+                        embed.addFields(
+                        { name:'Level', value: game.skywars.levelFormatted, inline: true },
+                        { name:'Prestige', value: game.skywars.prestige, inline: true },
+                        { name:'Played Games', value: game.skywars.playedGames, inline: true },
+                        { name:'Experience', value: game.skywars.experience, inline: true },
+                        { name:'XP Needed', value: game.skywars.levelProgress.xpToNextLevel, inline: true },
+                        { name:'Percent', value: game.skywars.levelProgress.percent, inline: true },
+                        { name:'Wins', value: game.skywars.wins, inline: true },
+                        { name:'Losses', value: game.skywars.losses, inline: true },
+                        { name:'WLR', value: game.skywars.WLRatio, inline: true },
+                        { name:'Kills', value: game.skywars.kills, inline: true },
+                        { name:'Deaths', value: game.skywars.deaths, inline: true },
+                        { name:'KDR', value: game.skywars.KDRatio, inline: true },
+                        { name:'Coins', value: game.skywars.coins, inline: true },
+                        { name:'Heads', value: game.skywars.heads, inline: true },
+                        { name:'Souls', value: game.skywars.souls, inline: true },
+                        { name:'Level Progress', value: genBar(game.skywars.levelProgress.percent), inline: false });
+                        break;
 
-				case 'bedwars':
-				case 'bw':
-					embed.setAuthor('Bedwars Stats');
-					embed.setDescription(nameFormatted);
-					embed.addFields(
-					{ name: 'Level', value: game.bedwars.level, inline: true },
-					{ name: 'Prestiege', value: game.bedwars.prestige, inline: true },
-					{ name: 'Total Games', value: game.bedwars.playedGames, inline: true },
-					{ name: 'Wins', value: game.bedwars.wins, inline: true },
-					{ name: 'Losses', value: game.bedwars.losses, inline: true },
-					{ name: 'WLR', value: game.bedwars.WLRatio, inline: true },
-					{ name: 'Kills', value: game.bedwars.kills, inline: true },
-					{ name: 'Deaths', value: game.bedwars.deaths, inline: true },
-					{ name: 'KDR', value: game.bedwars.KDRatio, inline: true },
-					{ name: 'Final Kills', value: game.bedwars.finalKills, inline: true },
-					{ name: 'Beds Broken', value: game.bedwars.beds.broken, inline: true },
-					{ name: 'Coins', value: game.bedwars.coins, inline: true },
-					);
-					break;
+                    case 'bedwars':
+                    case 'bw':
+                        embed.setAuthor('Bedwars Stats');
+                        embed.setDescription(nameFormatted);
+                        embed.addFields(
+                        { name: 'Level', value: game.bedwars.level, inline: true },
+                        { name: 'Prestiege', value: game.bedwars.prestige, inline: true },
+                        { name: 'Total Games', value: game.bedwars.playedGames, inline: true },
+                        { name: 'Wins', value: game.bedwars.wins, inline: true },
+                        { name: 'Losses', value: game.bedwars.losses, inline: true },
+                        { name: 'WLR', value: game.bedwars.WLRatio, inline: true },
+                        { name: 'Kills', value: game.bedwars.kills, inline: true },
+                        { name: 'Deaths', value: game.bedwars.deaths, inline: true },
+                        { name: 'KDR', value: game.bedwars.KDRatio, inline: true },
+                        { name: 'Final Kills', value: game.bedwars.finalKills, inline: true },
+                        { name: 'Beds Broken', value: game.bedwars.beds.broken, inline: true },
+                        { name: 'Coins', value: game.bedwars.coins, inline: true },
+                        );
+                        break;
 
-				default:
-					return message.channel.send({ embed: { color: '#51eb39', description: '**LIST OF AVAILABLE MINIGAMES:**\nskywars, sw\nbedwars, bw\n**DUELS COMING SOON**', footer: false, thumbnail: false } }); }
-				}
+                    default:
+                        return message.channel.send({ embed: { color: '#51eb39', description: '**LIST OF AVAILABLE MINIGAMES:**\nskywars, sw\nbedwars, bw\n**DUELS COMING SOON**', footer: false, thumbnail: false } }); }
+                    }
 			else {
-						embed.setAuthor(nameFormatted);
-						embed.addField('Level', player.level, true);
-					if (player.guild) {
-						embed.addField('Guild', `[${player.guild.name}](${encodeURI(`https://plancke.io/hypixel/guild/name/${player.guild.name}`)})`, true); }
-
-					embed.addField('Achievement Points', player.achievementPoints.toLocaleString(), true);
-					embed.addField('Karma', player.karma.toLocaleString(), true);
-				}
-
-				message.channel.send({ embed });
-
+                embed.setAuthor(nameFormatted);
+                embed.addField('Level', player.level, true);
+                if (player.guild) {
+                    embed.addField('Guild', `[${player.guild.name}](${encodeURI(`https://plancke.io/hypixel/guild/name/${player.guild.name}`)})`, true);
+                }
+				embed.addField('Achievement Points', player.achievementPoints.toLocaleString(), true);
+				embed.addField('Karma', player.karma.toLocaleString(), true);
+            }
+            message.channel.send({ embed });
 		}).catch(e => {
 			console.log('[ERROR] There was an error running stats.js' + '\n' + e);
 			return message.channel.send({ embed: { color: '#eb3939', description: 'There was an error running this command! Did you type in the player name correctly?' } });
-
-
 		});
 		message.channel.stopTyping();
 	},
@@ -116,4 +112,4 @@ function genBar(num) {
 	const filler = 20 - digit;
 	const bar = '[ ' + ('■ '.repeat(digit)) + ('□ '.repeat(filler)) + ']';
 	return bar;
-	}
+}
